4df21349e2322c01f7bc9bc1c5843c45d6dc2a6d
El__Michi <hablansito@gmail.com>
2025-04-07 08:59:04 -0300

    Create web.js

---
+++ commands/web.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'web',
+  description: 'Esta es mi web o portafolio',
+  execute(message) {
+    message.reply('🌐 Mi web/portafolio: https://elmichidev.github.io/portafolio/');
+  }
+};
